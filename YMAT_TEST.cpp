aa8e0eacc4e6f82b5d0e8676f05324b8589fcb62
Lacty <akira206@gmail.com>
2017-03-24 08:53:01 +0900

    update

--- YMAT_TEST.cpp
+++ YMAT_TEST.cpp
@@ -9,13 +9,13 @@ using Mat = TMatrix<double>;
 
 
 int main() {
-  Vec pos(1, 1, 0);
-  
+  Vec vec;
   Mat translate(Translation<double>(10, 0, 0));
-  // Mat rotate(Rotation<double>(0, 1, 0));
-  std::cout << translate << std::endl;
+  Mat rotate(Rotation<double>(0, M_PI, 0));
+
+  vec *= translate;
+  std::cout << vec << std::endl; // [10, 0, 0]  
   
-  pos *= translate;
-  
-  std::cout << pos << std::endl;
+  vec *= rotate;  
+  std::cout << vec << std::endl; // [-10, 0, 0]
 }
